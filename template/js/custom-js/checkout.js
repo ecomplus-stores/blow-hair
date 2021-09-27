ff6b02bd90b44564e614ccb779601a625a57426d
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-27 16:25:02 -0300

    chore: loja mestre

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -2,7 +2,6 @@
 import ecomCart from '@ecomplus/shopping-cart'
 ecomCart.once('change', () => {
     if (document.querySelectorAll('.cart .recommended-items').length) {
-        document.querySelector('.cart .col-md-5.col-lg-4').insertAdjacentElement('afterend', document.querySelector('.cart .recommended-items'))
-        
+        document.querySelector('.cart .col-md-5.col-lg-4').insertAdjacentElement('afterend', document.querySelector('.cart .recommended-items'))     
     }
 })
