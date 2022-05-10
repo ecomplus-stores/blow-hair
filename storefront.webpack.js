732e9a48bd5a8f4629d9e999283e1bca8c829d84
eduvlemes <edu.vlemes@gmail.com>
2022-05-10 16:30:44 -0300

    app-brindes 1.0

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,9 @@ module.exports = () => ({
     alias: {
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
       './html/AccountPoints.html': path.resolve(__dirname, 'template/js/custom-js/html/AccountPoints.html'),
-      './html/DiscountApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/DiscountApplier.html')
+      './html/DiscountApplier.html': path.resolve(__dirname, 'template/js/blow-gummies/html/DiscountApplier.html'),
+      './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/blow-gummies/js/DiscountApplier.js')
+      //'./html/DiscountApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/DiscountApplier.html')
       //'./js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/js/DiscountApplier.js')
     }
   }
