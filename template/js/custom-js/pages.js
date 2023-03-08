9e50918ad827eff9f1ed698fc1c1e6f24cef996f
eduvlemes <edu.vlemes@gmail.com>
2023-03-08 12:54:51 -0300

    form newsletter

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -4,3 +4,16 @@ if (storefront && storefront.context && storefront.context.resource === 'product
         window.location = '/app/#/cart/' 
     })
 }
+
+$(document).ready(function(){
+    $('#apx_form').closest('.custom-html').toggleClass('my-4 my-0');
+    $('#apx_form').closest('.custom-html').toggleClass('my-lg-5 my-lg-0');
+});
+
+$('#apx_form').submit(function(e){
+    e.preventDefault();
+    let name = $('#conversion-form-am-formulario-de-newsletter [name="name"]').val().length > 0 ? $('#conversion-form-am-formulario-de-newsletter [name="name"]').val() : 'Visitante';
+    $('#conversion-form-am-formulario-de-newsletter [name="name"]').val(name);
+    $('#conversion-form-am-formulario-de-newsletter [name="email"]').val($(this).find('[name="mail"]').val());
+    $('#conversion-form-am-formulario-de-newsletter').submit();
+});
