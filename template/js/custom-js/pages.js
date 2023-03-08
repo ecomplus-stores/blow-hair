46fd07765357ed1030208d16ebe393f108208f5d
Leonardo Matos <leomp120894@gmail.com>
2023-03-08 19:09:23 -0300

    Skip fetching "free shipping from" options"

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,19 +1,23 @@
 // Add your custom JavaScript for storefront pages here.
+
 if (storefront && storefront.context && storefront.context.resource === 'products') {
-    ecomCart.on('addItem', () => { 
-        window.location = '/app/#/cart/' 
-    })
+  ecomCart.on('addItem', () => { 
+    window.location = '/app/#/cart/' 
+  })
 }
 
+// comente a linha abaixo para buscar regras de frete grátis
+window.modulesToFetch = [{ endpoint: 'list_payments' }]
+
 $(document).ready(function(){
-    $('#apx_form').closest('.custom-html').toggleClass('my-4 my-0');
-    $('#apx_form').closest('.custom-html').toggleClass('my-lg-5 my-lg-0');
+  $('#apx_form').closest('.custom-html').toggleClass('my-4 my-0');
+  $('#apx_form').closest('.custom-html').toggleClass('my-lg-5 my-lg-0');
 });
 
 $('#apx_form').submit(function(e){
-    e.preventDefault();
-    let name = $('#conversion-form-am-formulario-de-newsletter [name="name"]').val().length > 0 ? $('#conversion-form-am-formulario-de-newsletter [name="name"]').val() : 'Visitante';
-    $('#conversion-form-am-formulario-de-newsletter [name="name"]').val(name);
-    $('#conversion-form-am-formulario-de-newsletter [name="email"]').val($(this).find('[name="mail"]').val());
-    $('#conversion-form-am-formulario-de-newsletter').submit();
+  e.preventDefault();
+  let name = $('#conversion-form-am-formulario-de-newsletter [name="name"]').val().length > 0 ? $('#conversion-form-am-formulario-de-newsletter [name="name"]').val() : 'Visitante';
+  $('#conversion-form-am-formulario-de-newsletter [name="name"]').val(name);
+  $('#conversion-form-am-formulario-de-newsletter [name="email"]').val($(this).find('[name="mail"]').val());
+  $('#conversion-form-am-formulario-de-newsletter').submit();
 });
