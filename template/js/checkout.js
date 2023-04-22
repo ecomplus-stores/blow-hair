54c0105d817de98ed6079df62dc61a43bd19c86b
Leonardo Matos <leomp120894@gmail.com>
2023-04-22 11:28:38 -0300

    Revert upsell with recommended (none) items

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -2,4 +2,4 @@ import '#template/js/checkout'
 import './custom-js/checkout'
 
 window.ecomPaymentApps = [1250, 111223, 1251, 108091]
-window.ecomRecommendationsType = 'related'
+// window.ecomRecommendationsType = 'related'
