f5d207701ae84c9880df8af249ed5f341ce7dd95
Leonardo Matos <leomp120894@gmail.com>
2023-05-18 21:26:10 -0300

    Copying `TheCart.html` and `EcCheckout.html`

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -11,9 +11,9 @@ module.exports = () => ({
       './html/DiscountApplier.html': path.resolve(__dirname, 'template/js/blow-gummies/html/DiscountApplier.html'),
       './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/blow-gummies/js/DiscountApplier.js'),
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js')
-      //'./html/DiscountApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/DiscountApplier.html')
-      //'./js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/js/DiscountApplier.js')
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
+      './html/TheCart.html': path.resolve(__dirname, 'template/js/custom-js/components/TheCart.html'),
+      './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html')
     }
   }
 })
