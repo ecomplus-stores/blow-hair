065f62fc07a6ba898914ef65a264248249af0403
Leonardo Matos <leomp120894@gmail.com>
2023-05-19 21:42:09 -0300

    Enable GTM additional purchase event

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -5,3 +5,4 @@ window.ecomPaymentApps = [1250, 111223, 1251, 108091]
 // window.ecomRecommendationsType = 'related'
 
 window.__customGTMVariantRegex = /\s[^\s]+( zero a[ç|c][ú|u]car)?$/i
+window.__sendGTMExtraPurchaseData = true
