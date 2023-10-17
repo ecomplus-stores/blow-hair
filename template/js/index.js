a432b2a76f2fb2b7874f983670d931e521285907
matheusgnreis <matheus@e-com.club>
2023-10-17 17:54:34 -0300

    chore: use lozad to load defer script

--- template/js/index.js
+++ template/js/index.js
@@ -1,5 +1,40 @@
 import '#template/js/'
 import './custom-js/pages'
+import lozad from 'lozad'
+
+async function loadAsync () {
+    // RD form
+  if (typeof window.setupRd === 'function') {
+    const observer = lozad(document.getElementById('am-formulario-de-newsletter-b61733cf81abef42e0c4'), {
+      rootMargin: '350px 0px',
+      threshold: 0,
+      load () {
+        const script = document.createElement('script')
+        script.src = 'https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js'
+        script.id = 'hs-script-loader'
+        script.async = true
+        script.onload = window.setupRd
+        document.getElementById('am-formulario-de-newsletter-b61733cf81abef42e0c4').appendChild(script)
+      }
+    })
+    observer.observe()
+  }
+
+    //popup
+   lozad(document.getElementById('popup-rd'), {
+    rootMargin: '350px 0px',
+    threshold: 0,
+    load () {
+      const script = document.createElement('script')
+      script.src = 'https://d335luupugsy2.cloudfront.net/js/loader-scripts/0541136d-12e0-496c-9058-0def4b5ad0d4-loader.js'
+      script.id = 'rd-popup'
+      script.async = true
+      document.getElementById('popup-rd').appendChild(script)
+    }
+  }).observe()
+
+}
+loadAsync()
 
 window.__customGTMVariantRegex = /\s[^\s]+( zero a[ç|c][ú|u]car)?$/i
 
