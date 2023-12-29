4c57ac1f7998d490ad3611bebb69db46d904759d
matheusgnreis <matheus@e-com.club>
2023-12-29 09:22:44 -0300

    chore: send email

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -55,8 +55,6 @@ $(document).ready(function(){
 
 $('#apx_form').submit(function(e){
   e.preventDefault();
-  let name = $('#conversion-form-am-formulario-de-newsletter [name="name"]').val().length > 0 ? $('#conversion-form-am-formulario-de-newsletter [name="name"]').val() : 'Visitante';
-  $('#conversion-form-am-formulario-de-newsletter [name="name"]').val(name);
   $('#conversion-form-am-formulario-de-newsletter [name="email"]').val($(this).find('[name="mail"]').val());
   $('#conversion-form-am-formulario-de-newsletter').submit();
 });
