ebc0978290004578718763bf403794af3c737745
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-02 11:00:22 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,7 +1,7 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
 
-window.ecomPaymentApps = [1250, 113537, 1251, 108091]
+window.ecomPaymentApps = [111223,1250, 113537, 1251, 108091]
 // window.ecomRecommendationsType = 'related'
 
 window.__customGTMVariantRegex = /\s[^\s]+( zero a[ç|c][ú|u]car)?$/i
