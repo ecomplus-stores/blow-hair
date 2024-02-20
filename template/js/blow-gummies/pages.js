c5266f5a6ad1e5167627b5342b4e6a8956523864
matheusgnreis <matheus@e-com.club>
2024-02-20 00:50:09 -0300

    chore: final adjusts

--- template/js/blow-gummies/pages.js
+++ template/js/blow-gummies/pages.js
@@ -1,9 +1,1 @@
 // Add your custom JavaScript for storefront pages here.
-
-const toggleButton = document.getElementById('mobile-search-btn');
-toggleButton.addEventListener('click', () => {
-  const instantSearchComponent = document.querySelector('[name="InstantSearch"]');
-  if (instantSearchComponent) {
-    instantSearchComponent.__vue__.toggleVisibility(); // Chama o método do componente Vue
-  }
-});
