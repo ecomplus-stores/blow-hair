0dabe403399b1fd06750857b9368b78c95b979b8
matheusgnreis <matheus@e-com.club>
2024-02-19 22:03:52 -0300

    feat: add faq option store

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
       './html/CartQuickview.html': path.resolve(__dirname, 'template/js/custom-js/html/CartQuickview.html'),
       './js/CartQuickview.js': path.resolve(__dirname, 'template/js/custom-js/js/CartQuickview.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
