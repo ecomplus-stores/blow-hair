64250fd7457b28c26a47502f8fb49a0161033510
Leonardo Matos <leomp120894@gmail.com>
2024-10-16 19:38:38 -0300

    Assets from external Bunny CDN

--- .functions/index.js
+++ .functions/index.js
@@ -3,5 +3,6 @@ const functions = require('firebase-functions')
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
+process.env.STOREFRONT_ASSETS = 'https://s1-blowhair.b-cdn.net'
 
 exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
