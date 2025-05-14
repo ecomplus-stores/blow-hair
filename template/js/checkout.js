cced24ab185ad7e561604433543dce69b83e223e
Leonardo Matos <leomp120894@gmail.com>
2025-05-14 17:34:17 -0300

    Updating payment apps sort

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,7 +1,7 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
 
-window.ecomPaymentApps = [100478, 1250, 113537, 111223, 1251, 108091]
+window.ecomPaymentApps = [112381, 100478, 1250, 113537, 111223, 1251, 108091]
 // window.ecomRecommendationsType = 'related'
 
 window.__customGTMVariantRegex = /\s[^\s]+( zero a[ç|c][ú|u]car)?$/i
