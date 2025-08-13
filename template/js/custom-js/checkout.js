a53017c0d548a59440c6f77e575bea110abb3bf7
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-08-13 10:12:13 -0300

    retirar grid

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -108,14 +108,3 @@ if (bid) {
     window.sessionStorage.setItem('ecomUtm', JSON.stringify(sessionUtm))
     window.localStorage.setItem('buzzlead', bid)
 }
-// Grid de avaliações no checkout
-const reviewsDiv = document.createElement('div');
-reviewsDiv.className = 'sk-ww-google-reviews';
-reviewsDiv.setAttribute('data-embed-id', '25587705');
-document.body.appendChild(reviewsDiv); // ou escolha um container específico
-
-// Adiciona dinamicamente o script SociableKIT
-const script = document.createElement('script');
-script.src = 'https://widgets.sociablekit.com/google-reviews/widget.js';
-script.defer = true;
-document.body.appendChild(script);
